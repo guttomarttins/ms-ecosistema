cdb1321739fd8385829dff729861ba7bd8b62e5f
Carlos Martins <carlos.martins@sulamerica.com.br>
2022-09-29 00:13:40 -0300

    restTemplate

--- hr-payroll/src/main/java/com/cams/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/cams/hrpayroll/service/PaymentService.java
@@ -1,13 +1,29 @@
 package com.cams.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.cams.hrpayroll.entities.Payment;
+import com.cams.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bobo", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		Worker worker = restTemplate.getForObject(workerHost + "workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
