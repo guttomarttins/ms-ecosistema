1fd966ce0e86b1a88891cd24feae0b84ebf3b2c9
Carlos Martins <carlos.martins@sulamerica.com.br>
2022-09-29 00:39:14 -0300

    feign client

---
+++ hr-payroll/src/main/java/com/cams/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,17 @@
+package com.cams.hrpayroll.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cams.hrpayroll.entities.Worker;
+
+@Component
+@FeignClient(name = "hr-worker", url="http://localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+}
