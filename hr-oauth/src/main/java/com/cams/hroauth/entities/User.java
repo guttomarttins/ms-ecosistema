843faa293f6a448d01b45cf6b080a15da82887a3
Carlos Martins <carlos.martins@sulamerica.com.br>
2023-01-05 18:14:36 -0300

    Add Long JWT

--- hr-oauth/src/main/java/com/cams/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/cams/hroauth/entities/User.java
@@ -1,24 +1,30 @@
 package com.cams.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User implements Serializable{
- 
-	private static final long serialVersionUID = 7018489483575017834L;
-	
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails, Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private Long id;
 	private String name;
 	private String email;
 	private String password;
+	
 	private Set<Role> roles = new HashSet<>();
-
+	
 	public User() {
 	}
-	
+
 	public User(Long id, String name, String email, String password) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -63,7 +69,10 @@ public class User implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -75,7 +84,42 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
